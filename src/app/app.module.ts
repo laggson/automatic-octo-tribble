fe5da5379c3794ddaea9179dc67faa4380a481a8
Milchmann <bluemlein4@web.de>
2017-04-12 22:47:04 +0200

    Willkommensbereich hinzugefügt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
+﻿import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { WillkommensbereichPage } from '../pages/willkommensbereich/willkommensbereich';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,7 +15,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    WillkommensbereichPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +26,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    WillkommensbereichPage
+
   ],
   providers: [
     StatusBar,
---
+++ src/pages/willkommensbereich/willkommensbereich.module.ts
@@ -0,0 +1,16 @@
+﻿import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { WillkommensbereichPage } from './willkommensbereich';
+
+@NgModule({
+  declarations: [
+    WillkommensbereichPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(WillkommensbereichPage),
+  ],
+  exports: [
+    WillkommensbereichPage
+  ]
+})
+export class WillkommensbereichModule {}
